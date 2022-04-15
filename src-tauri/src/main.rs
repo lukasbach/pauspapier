ac33bf132ed9a4d5bb57efe6a5c35b9accc238e8
Lukas Bach <lbach@outlook.de>
2022-04-15 16:41:51 +0200

    removed unused rust code

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,19 +8,14 @@
 fn make_screenshot(x: i32, y: i32, dir: String) -> String {
   use screenshots::Capturer;
   use std::{fs::File, io::Write, time::Instant};
-  use image::*;
-  use webp::*;
-  use std::path::Path;
-  use  tauri::api::dir::with_temp_dir;
 
-  println!("I was invoked from JS!");
+  println!("Starting screenshot");
   let start = Instant::now();
 
   let capturers = Capturer::screen_capturers();
 
   for capturer in capturers {
     if capturer.display_info.x == x && capturer.display_info.y == y {
-      println!("1capturer {:?}", capturer);
       let image = capturer.capture_screen().unwrap();
       let buffer = image.png();
       let display_id = capturer.display_info.id.to_string();
@@ -28,19 +23,11 @@ fn make_screenshot(x: i32, y: i32, dir: String) -> String {
       let pathclone = path.clone();
       let mut file = File::create(path).unwrap();
       file.write_all(&buffer[..]).unwrap();
+      println!("Saved screenshot in {:?}ms", start.elapsed());
       return pathclone.into();
     }
   }
 
-  // let capturer = Capturer::screen_capturer_from_point(100, 100).unwrap();
-  // println!("2capturer {:?}", capturer);
-
-  // let image = capturer.capture_screen().unwrap();
-  // let buffer = image.png();
-  // let mut file = File::create("capture_display_with_point.png").unwrap();
-  // file.write_all(&buffer[..]).unwrap();
-
-  println!("运行耗时: {:?}", start.elapsed());
   return String::from("");
 }
 
