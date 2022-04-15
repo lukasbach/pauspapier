271f7239f5d87e639dfc1a2d924448963af84dc8
Lukas Bach <lbach@outlook.de>
2022-04-15 15:50:46 +0200

    app scaffold

---
+++ src/area-select.tsx
@@ -0,0 +1,10 @@
+import { Area } from './common';
+import React from 'react';
+
+export const AreaSelect: React.FC<{ screenshotPath: string, onSelectArea: (area: Area) => void }> = props => {
+  return (
+    <>
+      Area Select
+    </>
+  );
+};
---
+++ src/common.ts
@@ -0,0 +1,7 @@
+export enum AppState {
+  StartPage,
+  AreaSelect,
+  InArea,
+}
+
+export interface Area {x: number, y: number, w: number, h: number}
